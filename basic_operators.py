26bfe9bf58bbc54eee9cb164cd947f463c3f1c11
jim walter <jimwalter13090@gmail.com>
2020-10-21 14:30:27 -0600

    basic operators

---
+++ basic_operators.py
@@ -0,0 +1,14 @@
+# +, -, *, //, **, %
+
+print(7 + 3.)  # addition of an int and float returns a float
+
+print(7 / 3)  # rounds off to 2.33333335; all division returns a float
+
+print(7 // 3)  #rounds down the result to int => 2
+
+print(2 ** 3)  # exponent => 8
+
+print(9 % 2)  # modulo returns remainder => 1 (great for even/odd)
+
+print(11 % 2 == 1)  # '==' checks for equality => True
+
