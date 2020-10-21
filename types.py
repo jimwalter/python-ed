c137393254644bd2f039c17b3f91c40e20cab479
jim walter <jimwalter13090@gmail.com>
2020-10-21 14:24:28 -0600

    int, float, bool into types.py

---
+++ types.py
@@ -0,0 +1,15 @@
+print(type(27)) #int
+
+print(type(33.5))
+'''
+float, true for large floating decimals and negatives as well
+division of any two int's will always be a data type = float
+'''
+
+print(type(True))  #boolean, needs to be capitalized
+
+print(type(7.))
+'''
+The presence of the decimal point signals that Python will interpret 7. as a float,
+and likewise for 7.0 and 7.0000000.
+'''
