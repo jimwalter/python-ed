cc14002dea7d477fabe6caa6e55a9054a1d7310b
jim walter <jimwalter13090@gmail.com>
2020-10-21 15:21:31 -0600

    printing and input() explored

---
+++ intro_to_python/python_basics/printing.py
@@ -0,0 +1,11 @@
+# using input() and print()
+some_str = 'printing'
+
+print('this is a string')
+
+print(f'this is {some_str} to the console')  # -> f print to add vars in print statement
+
+x = input('gimme input: ')
+print(x)  # -> will prompt you for input , which type string
+
+# to change type of input, say for an integer, type cast in the var declaration on line 8
