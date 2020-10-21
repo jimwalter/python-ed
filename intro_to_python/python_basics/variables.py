1e9219e65364da8e5ae2faa6e4945954337fb34e
jim walter <jimwalter13090@gmail.com>
2020-10-21 15:04:40 -0600

    file directory laid out for all lessons

---
+++ intro_to_python/python_basics/variables.py
@@ -0,0 +1,25 @@
+x = "some string"
+print(type(x))  # -> str
+
+'''
+    x is assigned to some... thing using the assingment operator, =
+
+'''
+
+# Simple variable assignment
+x = 7
+# Now you can refer to the integer value '7' using the name 'x'
+print(x)
+
+
+# Assign the result of an expression to a variable
+y = 521 / 74 + 7**(1/8)
+print(y)
+
+
+# Reference other variables in an assignment
+z = (x + y) / 100
+print(z)
+
+# INCREMENTATION
+
